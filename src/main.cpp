1a75aa50b6a2e8c6258cf87fb0f098a5cc1e7e1e
pedro-rodrigues18 <hrpereira.pedro@gmail.com>
2025-02-18 15:47:28 -0300

    feat: Adding the ability to run one or all instances at once

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <filesystem>
 #include <chrono>
 #include "../rapidjson/document.h"
 #include "../rapidjson/filereadstream.h"
@@ -8,9 +9,8 @@
 #include "optimization.hpp"
 #include "../utils/log.hpp"
 
-int main() {
-
-    string file_name = "A_08";
+void MakeOptimization(std::string instance) {
+    cout << "Running instance " << instance << endl;
 
     auto start_time = std::chrono::high_resolution_clock::now();
     std::time_t now_c = std::chrono::system_clock::to_time_t(start_time);
@@ -19,14 +19,14 @@ int main() {
     time_stream << std::put_time(std::localtime(&now_c), "%Y-%m-%d %X");
     std::string formatted_time = time_stream.str();
 
-    utils::Log(file_name, "Starting at " + formatted_time);
+    utils::Log(instance, "Starting at " + formatted_time);
 
     // Load the problem
-    FILE* fp = fopen(("input/" + file_name + ".json").c_str(), "r");
+    FILE* fp = fopen(("input/" + instance + ".json").c_str(), "r");
 
     if (!fp) {
-        utils::Log(file_name, "Error: Could not open file.");
-        return 1;
+        utils::Log(instance, "Error: Could not open file.");
+        exit(1);
     }
 
     char readBuffer[65536];
@@ -38,36 +38,62 @@ int main() {
     fclose(fp);
 
     if (doc.HasParseError()) {
-        utils::Log(file_name, "Error: Could not parse JSON.");
-        utils::Log(file_name, "Error code: " + to_string(doc.GetParseError()));
-        return 1;
+        utils::Log(instance, "Error: Could not parse JSON.");
+        utils::Log(instance, "Error code: " + to_string(doc.GetParseError()));
+        exit(1);
     }
 
-    Problem problem = Problem(&doc, file_name);
+    Problem problem = Problem(&doc, instance);
 
     auto elapsed_time = chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - start_time).count();
 
-    utils::Log(file_name, "Problem loaded successfully!");
-    utils::Log(file_name, "Elapsed time: " + to_string(elapsed_time) + "ms");
+    utils::Log(instance, "Problem loaded successfully!");
+    utils::Log(instance, "Elapsed time: " + to_string(elapsed_time) + "ms");
 
     // Optimization Step
     Optimization optimization = Optimization(&problem);
 
     vector<pair<string, int>> solution = optimization.OptimizationStep(start_time);
 
-    utils::Log(file_name, "Optimization finished!");
+    utils::Log(instance, "Optimization finished!");
     elapsed_time = chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - start_time).count();
 
-    utils::Log(file_name, "Elapsed time: " + to_string(elapsed_time) + "ms\n");
+    utils::Log(instance, "Elapsed time: " + to_string(elapsed_time) + "ms\n");
 
     // Write solution to txt file
-    ofstream output_file("output/" + file_name + ".txt");
+    ofstream output_file("output/" + instance + ".txt");
 
-    for (const auto& [name, start_time] : solution) {
-        output_file << name << " " << start_time << endl;
+    for (const auto& [intervention, start_time_execution] : solution) {
+        output_file << intervention << " " << start_time_execution << endl;
     }
 
     output_file.close();
+}
+
+void RunAllInstances(std::vector<std::string> instances) {
+    for (const auto& instance : instances) {
+        MakeOptimization(instance);
+    }
+}
+
+int main() {
+    bool run_all = false;
+    std::string input_path = "input/";
+    std::string instance = "";
+    std::vector<std::string> instances;
+
+    for (const auto& entry : std::filesystem::directory_iterator(input_path)) {
+        instance = entry.path().filename().string();
+        instances.push_back(instance.substr(0, instance.find('.')));
+    }
+
+    if (run_all) {
+        RunAllInstances(instances);
+    }
+    else {
+        instance = "A_09";
+        MakeOptimization(instance);
+    }
 
     return 0;
 }
